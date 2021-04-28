60b4723779c2e0e0c443563fbcb346cdda0a772e
ojacomarket <elfutbolesmivida@mail.ru>
2021-04-29 01:55:07 +0300

    Add converter from Geodetic to Cartesian and vice versa

---
+++ converter/src/main/java/ee/taltech/converter/GeodeticIntoCartesian.java
@@ -0,0 +1,19 @@
+package ee.taltech.converter;
+
+public class GeodeticIntoCartesian {
+    public static void main(String[] args) {
+        WGS84 tallinn = new WGS84();
+        Kaev_Left point_1 = new Kaev_Left();
+        point_1.calculate_cartesian_coordinates(tallinn, point_1);
+        System.out.println("Cartesian X is " + point_1.cartesian_x);
+        System.out.println("Cartesian Y is " + point_1.cartesian_y);
+        System.out.println("Cartesian Z is " + point_1.cartesian_z);
+        System.out.println();
+        System.out.println();
+        point_1.calculate_geodetic_coordinates(tallinn, point_1);
+        System.out.println("Geodetic latitude is " + point_1.find_latitude); // OK
+        System.out.println("Geodetic longitude is " + point_1.find_longitude); // OK
+        System.out.println("Geodetic height is " + point_1.find_height); // OK, if latitude is hardcoded
+    }
+}
+    
---
+++ converter/src/main/java/ee/taltech/converter/WGS84.java
@@ -0,0 +1,38 @@
+package ee.taltech.converter;
+
+import lombok.Data;
+
+/**
+ * Math.
+ *      cos
+ *      sin
+ *      tan
+ * They all operate on RADIANS, to pass RADIANS into equation do --> Math.cos(90 * Math.PI / 180) = 0.0
+ */
+@Data
+public class WGS84 {
+
+    //ok
+    private final double semi_major_axis_radius = 6378137D;
+    //ok
+    private final double semi_minor_axis_radius = 6356752D;
+    //ok
+    private double flattening = (semi_major_axis_radius - semi_minor_axis_radius) / semi_major_axis_radius;
+
+
+    //ok
+    private final double eccentricity_square = (Math.pow(semi_major_axis_radius,2) - Math.pow(semi_minor_axis_radius,2))
+            / Math.pow(semi_major_axis_radius,2);
+
+    private final double eccentricity_2_square = (Math.pow(semi_major_axis_radius,2) - Math.pow(semi_minor_axis_radius,2))
+            / Math.pow(semi_minor_axis_radius,2);
+
+    //ok
+    private final double eccentricity = Math.sqrt(eccentricity_square);
+
+
+    public double get_radius_of_curvature_on_prime_vertical (Kaev_Left kaevLeft, WGS84 earth) {
+        return earth.semi_major_axis_radius / (Math.sqrt
+                (1 - (earth.eccentricity_square * Math.pow(Math.sin(kaevLeft.getGeodetic_latitude() * Math.PI / 180), 2))));
+    }
+}
